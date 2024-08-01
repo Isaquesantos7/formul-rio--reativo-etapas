8e55d63063081ecf85bd33d9476cb168b7b57b9a
Isaquesantos7 <isaquesantos.second.1998@gmail.com>
2024-08-01 00:13:00 -0300

    feat: criando estrutura base do template

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
 
 export class FormComponent {
-
+  
 }
