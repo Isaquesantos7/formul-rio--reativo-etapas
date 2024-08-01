ad7520a9bfe6582b76b7c552fa28eed6d9b52478
Isaquesantos7 <isaquesantos.second.1998@gmail.com>
2024-08-01 00:51:00 -0300

    feat: Coletando dados digitados e submetendo

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,16 +1,70 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgFor
   ],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
 
 export class FormComponent {
+  protected form!: FormGroup;
   
+  constructor(private formBuildService: FormBuilder) {
+    this.form = this.formBuildService.group({
+      identificacao: this.formBuildService.group({
+        nome: ['', Validators.required],
+        codigoRegiao: ['', Validators.required]
+      }),
+
+      endereco: this.formBuildService.group({
+        rua: ['', Validators.required],
+        cidade: ['', Validators.required],
+        estado: ['', Validators.required],
+        cep: ['', Validators.required]
+      }),
+
+      conta: this.formBuildService.group({
+        agencia: ['', Validators.required],
+        numero: ['', Validators.required],
+        tipoConta: ['', Validators.required],
+        nomeBanco: ['', Validators.required]
+      }),
+
+      telefone: this.formBuildService.array([])
+    })
+  }
+
+  get telefone() {
+    return this.form.get('telefone') as FormArray;
+  }
+
+  public addTelefone(): void {
+    const setTelefone = this.formBuildService.group({
+      ddd: ['', Validators.required],
+      numeroTelefone: ['', Validators.required]
+    })
+
+    this.telefone.push(setTelefone);
+  }
+
+  public removeTelefone(index: number): void {
+    this.telefone.removeAt(index);
+  }
+
+  public onSubmit(): void {
+    if (this.form.value) {
+      return;
+    } 
+
+    console.log(this.form.value);
+
+    this.form.reset();
+  }
 }
